037c13894a2ee2088ba2eb6e6da29b7e512c0ce9
Vidya Sagar VOBBILISETTI <mail@vidyasagarv.com>
2025-10-09 16:48:23 +0200

    Only change what you understand

--- .config/firefox/larbs.js
+++ .config/firefox/larbs.js
@@ -9,39 +9,41 @@ user_pref("browser.urlbar.suggest.topsites", false); // [FF78+]
 // user_pref("browser.urlbar.suggest.history", false);
 
 // Do not prefill forms:
-user_pref("signon.prefillForms", false);
+// user_pref("signon.prefillForms", false);
 
 // Do not autocomplete in the URL bar:
-user_pref("browser.urlbar.autoFill", false);
+// user_pref("browser.urlbar.autoFill", false);
 
 // Enable the addition of search keywords:
-user_pref("keyword.enabled", true);
+// user_pref("keyword.enabled", true);
 
 // Allow access to http (i.e. not https) sites:
-user_pref("dom.security.https_only_mode", false);
+// user_pref("dom.security.https_only_mode", false);
 
 // Keep cookies until expiration or user deletion:
-user_pref("network.cookie.lifetimePolicy", 0);
+// user_pref("network.cookie.lifetimePolicy", 0);
 
-user_pref("dom.webnotifications.serviceworker.enabled", false);
+// user_pref("dom.webnotifications.serviceworker.enabled", false);
 
 // Disable push notifications:
-user_pref("dom.push.enabled", false);
+// user_pref("dom.push.enabled", false);
 
 // Disable the pocket antifeature:
 user_pref("extensions.pocket.enabled", false);
 
 // Don't autodelete cookies on shutdown:
-user_pref("privacy.clearOnShutdown.cookies", false);
+// user_pref("privacy.clearOnShutdown.cookies", false);
 
 // Enable custom userChrome.js:
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
 // This could otherwise cause some issues on bank logins and other annoying sites:
-user_pref("network.http.referer.XOriginPolicy", 0);
+// user_pref("network.http.referer.XOriginPolicy", 0);
 
 // Disable Firefox sync and its menu entries
 // user_pref("identity.fxaccounts.enabled", false);
 
 // Fix the issue where right mouse button instantly clicks
 user_pref("ui.context_menus.after_mouseup", true);
+
+user_pref("browser.toolbars.bookmarks.visibility", "never");
