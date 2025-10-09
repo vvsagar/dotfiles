b3f1270a9947ac266ee53e4d5c8464a1c6cc0b75
Vidya Sagar VOBBILISETTI <mail@vidyasagarv.com>
2025-10-09 17:05:11 +0200

    Some more firefox prefs that I use

--- .config/firefox/user.js
+++ .config/firefox/user.js
@@ -40,10 +40,19 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // This could otherwise cause some issues on bank logins and other annoying sites:
 // user_pref("network.http.referer.XOriginPolicy", 0);
 
-// Disable Firefox sync and its menu entries
-// user_pref("identity.fxaccounts.enabled", false);
-
 // Fix the issue where right mouse button instantly clicks
 user_pref("ui.context_menus.after_mouseup", true);
 
+// Do not show bookmarks at the top
 user_pref("browser.toolbars.bookmarks.visibility", "never");
+
+// Clean interface
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.theme.content-theme", 0);
+user_pref("browser.theme.toolbar-theme", 0);
+
+// Get rid of ugly firefox sidebar, using sideberry instead
+user_pref("sidebar.visibility", "hide-sidebar");
